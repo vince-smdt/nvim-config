3cbe4d6437087729684c1892331e05a73c4fc136
vince-smdt <vince.smdt@gmail.com>
2026-01-17 00:05:19 -0500

    Add mapping to show full diagnostic messages

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -5,11 +5,13 @@ vim.keymap.set("n", "<leader>fe", vim.cmd.Ex)
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 vim.keymap.set("i", "{<CR>", "{<CR>}<Esc>O", { noremap = true })
+
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+vim.keymap.set("n", "<leader>o", vim.diagnostic.open_float, { desc = "Open diagnostic float" })
 
 -- DESKTOP GCS (Sirius)
 
