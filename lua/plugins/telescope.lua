9db26a9b1fa0ae2093721e62762ab90da10d4e5a
Vincent Simard-Schmidt <vince.smdt@gmail.com>
2026-04-04 14:12:29 -0400

    Show hidden files in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,6 +14,11 @@ return {
 	},
 	config = function()
 		require("telescope").setup({
+			pickers = {
+				find_files = {
+					hidden = true,
+				},
+			},
 			extensions = {
 				["ui-select"] = {
 					require("telescope.themes").get_dropdown(),
