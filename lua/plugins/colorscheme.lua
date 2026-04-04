e6e50d66d5e65b9d14b29c8819448351e36667bf
Vincent Simard-Schmidt <vince.smdt@gmail.com>
2026-04-04 17:01:30 -0400

    Allow transparent backgrounds in color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,6 +4,9 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
+		require("everforest").setup({
+			transparent_background_level = 1,
+		})
 		vim.cmd([[colorscheme everforest]])
 	end,
 }
