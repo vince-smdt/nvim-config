dcb561757b0d3f7287d0bff7becae1370618528b
Vincent Simard-Schmidt <vince.smdt@gmail.com>
2026-05-09 18:47:48 -0400

    Move plugins to 'plugin' folder, add status bar

--- init.lua
+++ init.lua
@@ -1,1 +1,6 @@
-require("config")
+require("options")
+require("remap")
+require("autocmd")
+require("schedule")
+
+GH = "https://github.com/"
---
+++ plugin/status_bar.lua
@@ -0,0 +1,10 @@
+vim.pack.add({
+	"https://github.com/nvim-tree/nvim-web-devicons",
+	"https://github.com/nvim-lualine/lualine.nvim",
+})
+
+local everforest = require("lualine.themes.everforest")
+
+require("lualine").setup({
+	options = { theme = everforest },
+})
