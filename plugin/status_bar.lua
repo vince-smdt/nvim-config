c715fa241e3d1e2f1c69e6b5f72914d0f3bf8920
vince-smdt <vince.smdt@gmail.com>
2026-06-09 18:01:14 -0400

    Change status bar filename to be relative path

--- plugin/status_bar.lua
+++ plugin/status_bar.lua
@@ -10,7 +10,7 @@ require("lualine").setup({
 	sections = {
 		lualine_a = { "mode" },
 		lualine_b = { "branch" },
-		lualine_c = { "filename" },
+		lualine_c = { { "filename", path = 1 } },
 		lualine_x = { "lsp_status" },
 		lualine_y = { "progress" },
 		lualine_z = { "location" },
